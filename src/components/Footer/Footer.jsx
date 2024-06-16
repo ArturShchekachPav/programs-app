44c12202f5b7276b0fe8292ba266e0be336853a6
Artur Shchekach <arturshchekatch@yandex.ru>
2024-06-16 16:18:06 +1000

    fix: vendors and adaptive layout

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <footer className="footer">
             <ul className="footer__info">
-                <li className="footer__item footer__item_rights">2018-2023 Платформа, все права защищены</li>
+                <li className="footer__item footer__item_rights">&#169; 2018-2023 Платформа, все права защищены</li>
                 <li className="footer__item"><a href="#" className="footer__link hover">Контактная информация</a></li>
                 <li className="footer__item"><a href="#" className="footer__link hover">Конфиденциальность</a></li>
                 <li className="footer__item"><a href="#" className="footer__link hover">Пользовательское соглашение</a>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "normalize.css";
 import './index.css';
+import "@fontsource/open-sans/400.css";
+import "@fontsource/open-sans/600.css";
+import "@fontsource/montserrat/700.css";
 import App from './components/App/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
